d09e8ece0049b35453e7de7fc83cabe9383c2b17
Kim Robert <99035219+kimt0t0@users.noreply.github.com>
2022-10-18 12:14:33 +0200

    add nodemon module feat secondpage

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-    res.end("Réponse du serveur :-)")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome !</h1><br/><a href='./toto'>Visiter la page de Toto</a>")
 })
 
-server.listen(process.env.PORT || 3000)
+app.get('/toto', (req, res) => {
+    res.send("<h1>Voici la page de Toto!</h1><br/><a href='../'>Revenir à l'accueil</a>")
+})
+
+app.listen(port, () => {
+    console.log(`Application de l'exemple en cours d'exécution sur le port ${port}`)
+})
